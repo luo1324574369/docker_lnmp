9efc2dfdd8580c1f414077eb66d3e66d066e141f
jinmiluo <1324574369@qq.com>
2020-04-17 14:31:07 +0800

    增加一个swoole

--- www/swoole/server.php
+++ www/swoole/server.php
@@ -6,7 +6,7 @@
  * Time: 10:14
  */
 
-$http = new Swoole\Http\Server("127.0.0.1", 9501);
+$http = new Swoole\Http\Server("0.0.0.0", 9501);
 $http->on('request', function ($request, $response) {
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
