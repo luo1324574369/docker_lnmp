4253bb73702e3b22631b575db5570fe5814949d9
jinmi luo <luo1324574369@gmail.com>
2020-04-17 10:25:16 +0800

    增加swoole

--- www/www/swoole/server.php
+++ www/www/swoole/server.php
@@ -6,4 +6,8 @@
  * Time: 10:14
  */
 
-echo phpinfo();
+$http = new Swoole\Http\Server("127.0.0.1", 9501);
+$http->on('request', function ($request, $response) {
+    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+});
+$http->start();
